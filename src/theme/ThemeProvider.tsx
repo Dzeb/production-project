e1c77227b894b28a51fed9cbf7829517eac28875
Den <grour@inbox.ru>
2023-05-21 17:24:09 +0300

    7. Add themes

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,25 @@
+import { Children, FC, useMemo, useState } from "react";
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
+
+const defalutTheme =
+  (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
+
+const ThemeProvider: FC = ({ children }) => {
+  const [theme, setTheme] = useState<Theme>(defalutTheme);
+
+  const defaultProps = useMemo(
+    () => ({
+      theme: theme,
+      setTheme: setTheme,
+    }),
+    [theme]
+  );
+
+  return (
+    <ThemeContext.Provider value={defaultProps}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
