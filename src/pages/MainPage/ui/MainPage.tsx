dd792ebb47586364f38bf001370ad4ac66bd9315
Den <grour@inbox.ru>
2024-03-24 17:35:40 +0300

    30 fix storybook

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,5 +1,4 @@
 import { BugButton } from 'app/providers/ErrorBoundary';
-import { Counter } from 'entities/Counter';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
@@ -9,7 +8,6 @@ const MainPage = () => {
     <div>
       <BugButton />
       {t('Главная')}
-      <Counter />
     </div>
   );
 };
