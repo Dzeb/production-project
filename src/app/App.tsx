dbc153c89acab4599838575a275b9c030bbda69d
Den <grour@inbox.ru>
2024-08-05 01:01:00 +0300

    47 update app route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,15 @@ import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense, useEffect } from 'react';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
-import { userActions } from 'entities/User';
+import { getUserIninted, userActions } from 'entities/User';
+import { useSelector } from 'react-redux';
 import { AppRouter } from './providers/router';
 import { useTheme } from './providers/ThemeProvider';
 
 function App() {
   const { theme } = useTheme();
   const dispatch = useAppDispatch();
+  const inited = useSelector(getUserIninted);
 
   useEffect(() => {
     dispatch(userActions.initAuthData());
@@ -21,7 +23,7 @@ function App() {
         <Navbar />
         <div className='content-page'>
           <Sidebar />
-          <AppRouter />
+          {/* {inited && <AppRouter />} */}
         </div>
       </Suspense>
     </div>
--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -3,3 +3,5 @@ export { userReducer, userActions } from './model/slice/userSlice';
 export { UserSchema, User } from './model/types/user';
 
 export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData';
+
+export { getUserIninted } from './model/selectors/getUserInited/getUserInited';
